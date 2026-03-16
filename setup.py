9e1657ae608e152805a74d81c0c8073009105482
iuyehara <43707549+iuyehara@users.noreply.github.com>
2026-03-16 16:37:37 -0700

    Update setup.py
    added package versions

--- setup.py
+++ setup.py
@@ -6,17 +6,16 @@ setup(
     description='A tool for stitching together images from agricultural scenes using LightGlue and OpenCV',
     author='GEMINI Breeding',
     author_email='ikuyehara@ucdavis.edu',
-    url='https://github.com/GEMINI-Breeding/Panorama-Maker.git',  # Replace with your repo
+    url='https://github.com/iuyehara/AgRowStitch',
     packages=find_packages(),
     install_requires=[
-        'ipykernel',
-        'numpy',
-        'opencv-python',
-        'pandas',
-        'PyYAML',
-        'scipy',
-        'torch',
-        'torchvision'
+        'numpy==2.2.6',
+        'opencv-python==4.10.0.84',
+        'pandas==2.2.3',
+        'PyYAML==6.0.2',
+        'scipy==1.14.1',
+        'torch==2.5.1+cu118',
+        'torchvision==0.20.1'
     ],
     dependency_links=[
         'https://github.com/cvg/LightGlue.git#egg=LightGlue'
